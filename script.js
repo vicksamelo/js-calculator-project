f4c30b63c3bd9f727f5263eeb683e70cb1eada18
Victória Melo <contato@victoriamelo.com>
2022-05-18 14:25:34 -0300

    progress in js calculator

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
 const num = document.getElementsByClassName('num');
 const numArray = Array.from(num);
-
+const clear = document.getElementById('clear');
 const ops = document.getElementsByClassName('ops');
 const opsArray = Array.from(ops);
-
-numArray.forEach((number) => {
-  number.addEventListener('click', () => {
-    console.log(number.innerHTML);
-  });
-});
+const viewer = document.getElementById('viewer');
+let result = 0;
+let currentOp;
+let firstNumber;
+let secondNumber = 0;
 
 opsArray.forEach((op) => {
   op.addEventListener('click', () => {
-    console.log(op.innerHTML);
+    console.log(op.textContent);
+    viewer.textContent += op.textContent;
+    currentOp = op.textContent;
   });
+});
+
+numArray.forEach((number) => {
+  //Mostra quando cada botão for clicado
+  number.addEventListener('click', () => {
+    if (firstNumber === undefined) {
+      //Atribui o numero clicado a variavel do primeiro numero
+      firstNumber = parseInt(number.textContent);
+
+      //Imprime no visor o numero clicado
+      viewer.textContent = firstNumber;
+      console.log(firstNumber);
+      return;
+    }
+    //Atribui o numero clicado a variavel do primeiro numero
+    let secondNumber = parseInt(number.textContent);
+    console.log(secondNumber);
+    console.log(currentOp);
+
+    if (currentOp == '*') {
+      result = firstNumber * secondNumber;
+      console.log(result);
+    } else if (currentOp == '/') {
+      result = firstNumber / secondNumber;
+      console.log(result);
+    } else if (currentOp == '+') {
+      result = firstNumber + secondNumber;
+      console.log(result);
+    } else if (currentOp == '-') {
+      result = firstNumber - secondNumber;
+      console.log(result);
+    }
+    //Imprime no visor o numero clicado
+    viewer.textContent = result;
+  });
+});
+
+//Botão de limpar
+clear.addEventListener('click', () => {
+  result.textContent = 0;
+  viewer.textContent = result.textContent;
+  firstNumber.textContent = 0;
+  secondNumber.textContent = 0;
+  console.log(result.textContent);
+  console.log(firstNumber.textContent);
+  console.log(secondNumber.textContent);
 });
